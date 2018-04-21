d6bff2c901807af4b703860b5a3909304f03945d
Dylan Dewald <dylan.dewald94@gmail.com>
2018-04-21 18:34:39 -0400

    Started pattern implementation.

--- School/src/cps162/assignments/pattern/Pattern.java
+++ School/src/cps162/assignments/pattern/Pattern.java
@@ -1,5 +1,52 @@
 package cps162.assignments.pattern;
 
 public class Pattern {
-
+	
+	
+	
+	public static void main(String []args){
+		String pattern = getPattern(7);
+		pattern = makePrettyPattern(pattern, 1);
+		System.out.println(pattern);
+	}
+	
+	//vvvvvvvvv Not implemented yet vvvvvvvvvvvvv
+	public static void printPattern(int n){
+		if(n <= 5){
+			System.out.println(getPattern(n));
+		}
+		else{
+			String prevPattern = getPattern(n-1);
+			System.out.println(prevPattern);
+			System.out.println(getPattern(n));
+			System.out.println(prevPattern);
+		}
+	}
+	
+	//Constructs pattern perfectly
+	private static String getPattern(int caseNum){
+		String pattern = null;
+		if(caseNum == 1){
+			return "1";
+		}
+		else if(caseNum > 1){
+			String prevPattern = getPattern((caseNum - 1));
+			pattern = prevPattern + " " + caseNum + " " + prevPattern; 
+		}
+		return pattern;
+	}
+	// vvvvvvvvvvvvvvvvv does not work exactly right vvvvvvvvvvvvvvvvv
+	private static String makePrettyPattern(String pattern, int returnIndex){
+		int length = pattern.length();
+		int nextLineNum = returnIndex * 64;
+		if(nextLineNum > length){
+			return pattern;
+		}
+		else{
+			pattern = makePrettyPattern(pattern, (returnIndex + 1));
+			char[] sequence = pattern.toCharArray() ;
+			sequence[nextLineNum] = '\n';
+			return String.valueOf(sequence);
+		}
+	}
 }
