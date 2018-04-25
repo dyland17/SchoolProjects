b9d5693bd221528d7b43cc2e05faa26d9a445001
Dylan Dewald <dylan.dewald94@gmail.com>
2018-04-25 14:03:23 -0400

    Getting closer to making the positive recursion part of this working.

--- School/src/cps162/assignments/commas/Commas.java
+++ School/src/cps162/assignments/commas/Commas.java
@@ -3,21 +3,38 @@ package cps162.assignments.commas;
 public class Commas {
 		
 		public static void main(String []args){
-			int number = 1000000000;
-			formatLongWithCommas(number);
-			System.out.println(number);
+			int number = 50125;
+			String output = formatLongWithCommas(number);
+			System.out.println(output);
 		}
 	
 	
 		public static String formatLongWithCommas(long number){
-			String formattedNumber;
+			String formattedNumber = null;
+			long shortenedNumber;
+			long modNumber = number % 1000;
+			// Returns anything that is under a thousand.
 			if(number < 1000){
-				return String.valueOf(number); 
+				formattedNumber = String.valueOf(number); 
+				return formattedNumber;
 			}
 			else{
-				//formatLongWithCommas(dividedNumber);
+					if(modNumber == 0){
+					shortenedNumber = number /1000;
+					formattedNumber = shortenedNumber + "," + "000";
+				}
+				else{
+					long differenceNumber = number - modNumber;
+					shortenedNumber = differenceNumber /1000;
+					formattedNumber = shortenedNumber + "," + modNumber;
+				}
 			}
-			return null;
+				
+		
+			
+			
+			return formattedNumber;
+			//formatLongWithCommas(dividedNumber);
 		}
 		private static int findHowManyCommasNeeded(long number){
 			return findHowManyCommasNeeded(number, 1000, 0);
