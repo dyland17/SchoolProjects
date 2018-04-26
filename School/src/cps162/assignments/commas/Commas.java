3737efaafd3387cf4ba31685a01d9176f64c9333
Dylan Dewald <dylan.dewald94@gmail.com>
2018-04-25 22:37:32 -0400

    Finished commas class.

--- School/src/cps162/assignments/commas/Commas.java
+++ School/src/cps162/assignments/commas/Commas.java
@@ -1,45 +1,94 @@
 package cps162.assignments.commas;
-
+import java.util.Scanner;
+/**
+ *  This class contains a method that will take any long value from the user and 
+ *  provide a comma formatted value back.
+ * @author Dylan Dewald
+ */
 public class Commas {
 		
 		public static void main(String []args){
-			int number = 185700125;
-			String output = formatLongWithCommas(number);
-			System.out.println(output);
+			long number;
+			String numberFormatted;
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Write a number to be formatted with commas?");
+			while(scan.hasNextLong()){
+				number = scan.nextLong();
+				numberFormatted = formatLongWithCommas(number);
+				System.out.println(numberFormatted);
+			}
 		}
 	
-	
+		/**
+		 * 
+		 * @param number long value to be formatted with commas.
+		 * @return Will return a String representing the long value with commas.
+		 */
 		public static String formatLongWithCommas(long number){
 			String formattedNumber = null;
 			// Returns anything that is under a thousand.
-			if(number < 1000){
-				formattedNumber = String.valueOf(number); 
-				return formattedNumber;
-			}
-			else{
-				long shortenedNumber = number / 1000;
-				long modNumber = number % 1000;
-				if(modNumber == 0){
-					formattedNumber = formatLongWithCommas(shortenedNumber) + ",000";
+			if(number > 0){
+				if(number < 1000){
+					formattedNumber = String.valueOf(number); 
+					return formattedNumber;
 				}
 				else{
-					formattedNumber = formatLongWithCommas(shortenedNumber) + "," + modNumber;
+					long shortenedNumber = number / 1000;
+					long modNumber = number % 1000;
+					if(modNumber >= 100){
+						if(modNumber == 0){
+							formattedNumber = formatLongWithCommas(shortenedNumber) + ",000";
+						}
+						else{
+							formattedNumber = formatLongWithCommas(shortenedNumber) + "," + modNumber;
+						}
+					}
+					else{
+						if(modNumber == 0){
+							formattedNumber = formatLongWithCommas(shortenedNumber) + ",000";
+						}
+						else{
+							if(modNumber < 10){
+								formattedNumber = formatLongWithCommas(shortenedNumber) + ",00" + modNumber;
+							}
+							else{
+								formattedNumber = formatLongWithCommas(shortenedNumber) + ",0" + modNumber;
+							}
+						}
+					}
+				}
+			}
+			else{
+				if(number > -1000){
+					formattedNumber = String.valueOf(number); 
+					return formattedNumber;
+				}
+				else{
+					long shortenedNumber = number / 1000;
+					long modNumber = number % 1000;
+					if(modNumber <= -100){
+						if(modNumber == 0){
+							formattedNumber = formatLongWithCommas(-shortenedNumber) + ",000";
+						}
+						else{
+							formattedNumber = formatLongWithCommas(shortenedNumber) + "," + -modNumber;
+						}
+					}
+					else{
+						if(modNumber == 0){
+							formattedNumber = formatLongWithCommas(-shortenedNumber) + ",000";
+						}
+						else{
+							if(modNumber > -10){
+								formattedNumber = formatLongWithCommas(shortenedNumber) + ",00" + -modNumber;
+							}
+							else{
+								formattedNumber = formatLongWithCommas(shortenedNumber) + ",0" + -modNumber;
+							}
+						}
+					}
 				}
 			}
 			return formattedNumber;
-			//formatLongWithCommas(dividedNumber);
-		}
-		private static int findHowManyCommasNeeded(long number){
-			return findHowManyCommasNeeded(number, 1000, 0);
-		}
-		private static int findHowManyCommasNeeded(long number, long checkNumber,int commasNeeded){
-			
-			if(checkNumber <= number){
-				commasNeeded++;
-				return findHowManyCommasNeeded(number, (checkNumber*1000), commasNeeded);
-			}
-			else{
-				return commasNeeded;
-			}
 		}
 }
